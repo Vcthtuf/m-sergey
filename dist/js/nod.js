4e17f1feba1b504b77cc58a541eac95c02082909
Vcthtuf <Vcthtuf@gmail.com>
2021-03-08 21:27:55 +0500

    Ready Tabs on page nod.html

--- dist/js/nod.js
+++ dist/js/nod.js
@@ -3,14 +3,56 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict'
 
     let blockInput = document.querySelector('.solution_input'),
-        input = document.querySelectorAll('.solution_input > input');
-    console.log(input);
+        blockMenu = document.querySelector('.nav_tabs > ul'),
+        tabs = document.querySelectorAll('.tab'),
+        tabContent = document.querySelectorAll('section');
+
+    // Очистка inputs при фокусе
 
     blockInput.addEventListener('click', function (event) {
         let target = event.target;
-        console.log(event.target);
         target.setAttribute('placeholder', '');
     });
+
+    // -------------------------
+
+    // Tabs
+
+    blockMenu.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('tab')) {
+
+            for (let i = 0; i < tabs.length; i++) {
+                if (target == tabs[i]) {
+                    console.log(target);
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+
+    });
+
+
+    function hideTabContent(a) {                        // функция скрытия контента
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {                        // функция показа контента
+        for (let i = 0; i < tabContent.length; i++) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+
+
 
 
 });
