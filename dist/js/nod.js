deeb23b10d3bd357edefc305a5afdcac1513a4bc
Vcthtuf <Vcthtuf@gmail.com>
2021-03-14 18:05:08 +0500

    Made calculate nod

--- dist/js/nod.js
+++ dist/js/nod.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    'use strict'
+    'use strict';
 
     let blockInput = document.querySelector('.solution_input'),
         blockMenu = document.querySelector('.nav_tabs > ul'),
@@ -25,6 +25,7 @@ window.addEventListener('DOMContentLoaded', function () {
             for (let i = 0; i < tabs.length; i++) {
                 if (target == tabs[i]) {
                     console.log(target);
+                    // target.classList.add('active');
                     hideTabContent(0);
                     showTabContent(i);
                     break;
@@ -37,19 +38,91 @@ window.addEventListener('DOMContentLoaded', function () {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
+            tabs[i].classList.remove('active');
         }
     }
 
     hideTabContent(1);
+    tabs[0].classList.add('active');
 
     function showTabContent(b) {                        // функция показа контента
         for (let i = 0; i < tabContent.length; i++) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
+            tabs[b].classList.add('active');
+
         }
     }
 
     // ---------------------------------------------
+
+    // Расчет НОД 
+
+    let inputValue = [],
+        inputItems = document.querySelectorAll('.solution_input > input'),
+        calcBtn = document.querySelector('.calc'),
+        inputResult = document.querySelector('.solution_result > input');
+
+    let nod = function (a, b) {
+        if (!b) {
+            return a;
+        }
+        return nod(b, a % b);
+    }
+
+    calcBtn.addEventListener('click', function () {
+        for (let i = 0; i < inputItems.length; i++) {
+            inputValue[i] = +inputItems[i].value;
+        }
+        inputResult.value = nod(...inputValue);
+    });
+
+    // ------------------------------------------------
+
+    // функция разложения на простые множители
+
+    function fact(number) {
+        let b = 2,
+            simpleNumbers = [];
+        while (number > b) {
+            while (number % b == 0) {
+                number /= b;
+                simpleNumbers.push(+b);
+            }
+            b++;
+            if (number == b) {
+                simpleNumbers.push(+b);
+            }
+        }
+        return simpleNumbers;
+    };
+
+    // ----------------------------------
+
+    // функция вывода решения
+
+
+
+    let solutionText = document.querySelector('.solution_text'),
+        solutionBtn = document.querySelector('.solution_btn');
+
+    solutionBtn.addEventListener('click', function () {
+        solutionText.insertAdjacentHTML('afterbegin', '<h3> Решение </h3>');
+        for (let i = 0; i <= inputValue.length; i++) {
+
+            solutionText.append(fact(inputValue[i]));
+            solutionText.append('<br>');
+        }
+    });
+
+
+    // solutionText.append(fact(36));
+
+    console.log(fact(54));
+    console.log(inputValue);
+    // console.log(simpleNumbers);
+
+
 
 
 
