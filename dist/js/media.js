0b682b405b07170f72277f04be47ccc9d9a95a7c
Vcthtuf <Vcthtuf@gmail.com>
2021-10-21 14:26:47 +0300

    Added event for click outside the hamburger

--- dist/js/media.js
+++ dist/js/media.js
@@ -4,8 +4,31 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let hamburger = document.querySelector('.hamburger'),
         aside = document.querySelector('.aside');
-    hamburger.addEventListener("click", function () {
+
+    hamburger.addEventListener('click', function() {
         aside.classList.toggle("aside_active");
         hamburger.classList.toggle("hamburger_active");
     });
+
+    window.addEventListener('click', e => { // при клике в любом месте окна браузера
+    const target = e.target // находим элемент, на котором был клик
+    if (!target.closest('.hamburger') && !target.closest('.aside')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+      asidePassive();
+    }
+  })
+
+    function asideActive() {
+        aside.classList.add("aside_active");
+        hamburger.classList.add("hamburger_active");
+    }
+
+    function asidePassive() {
+        aside.classList.remove("aside_active");
+        hamburger.classList.remove("hamburger_active");
+    }
+    
+
+
+
+ 
 });
